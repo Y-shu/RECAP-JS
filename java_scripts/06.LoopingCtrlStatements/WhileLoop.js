e712412ce44b26239cd0ede9d228f70fdf44da62
Y-shu <nivik12292@gmail.com>
2019-04-09 12:14:34 +0530

    Deleted Stuff

--- java_scripts/06.LoopingCtrlStatements/WhileLoop.js
+++
@@ -1,7 +0,0 @@
-var h=1;
-while(h<10){
-    document.write("<h3>"+h+"</h3>");
-    h=h+2;
-}
-//while loop we use when we are not sure about
-//no.of iterations...
--- java_scripts/PRAC/obj1.js
+++
@@ -1,9 +0,0 @@
-var user={
-    name:"yashu",
-    age:26
-}
-console.log(user.name);
-y=user;
-console.log(y);
-user.hello="HelloWorld";
-console.log(y);
