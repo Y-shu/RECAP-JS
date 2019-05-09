543e80b93f9ce69c622e7a899924d094deba7879
Y-shu <nivik12292@gmail.com>
2019-05-09 13:00:54 +0530

    Some

---
+++ AdvancedConceptsJ.S/05.Closures&Scope/windowObject.js
@@ -0,0 +1,7 @@
+//Global object holds all global variables
+//the root object or the global object depends on which run time you are using
+//for example: fire fox or chrome...
+//NOTE:When we run javascript in browser the global object is "window" object
+//what ever the variables that we create they get created at global level in global
+//object called---WINDOW....Same with properties...
+
