35af44b512c60d5a4ab4b70f86d64aed395d63c5
Y-shu <nivik12292@gmail.com>
2019-05-24 22:31:55 +0530

    Compilation and Interpretation...

---
+++ AdvancedConceptsJ.S/05.Closures&Scope/Compilation&Interpretation.js
@@ -0,0 +1,16 @@
+/*JAVASCRIPT is both compiled and Interpreted Language...
+1.What is Interpretation?
+The run time executes the code directly without and compilation step
+2.Compilation actaully converts source code into an intermediatory code
+or even the final object code
+3.In javascript source code which is written by us is executed by browser.
+4.But the is also an compilation step browser does before it acutally executes it
+5.Actually when the browser gets the execution code there are two steps that the browser
+executes:
+    First Step:Is the compilation (browser looking at your source code and identifying
+        a set of things which it needs inorder to execute it)
+        **but point here is not to generate the intermediatory file but to look 
+        for signs and to make note of certain things which it needs to execute it
+    Second Step:Interpretation step--where actually code is executed
+6.These two steps executed very quickly 
+ */
